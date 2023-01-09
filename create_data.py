80ae3f4f4523b5199cacce59b3e0d422077b4fa6
Aleksandr Dekunov <wresda@gmail.com>
2023-01-09 17:54:39 +0300

    f'

---
+++ create_data.py
@@ -0,0 +1,28 @@
+import csv
+import random
+from faker import Faker
+
+fake = Faker('ru_RU') # создаём класс и говорим что надо русский
+#print(fake.name(), fake.city_name(), fake.large_company())
+
+def get_fake_row():
+    '''создаёт рандомные данные через flaker'''
+    return [fake.name(), fake.city_name(), fake.street_address(),
+    fake.large_company(),
+    fake.job(), fake.phone_number(), fake.free_email(),
+    fake.date_of_birth(minimum_age=18, maximum_age=70),
+    random.randint(20000, 200000)]
+
+
+def generate_data(num_rows: int=100) -> None:
+    '''функция принимает то кол-во строк которое будет сгенерировано
+    записываем все данные в файл в формате csv'''
+    with open('salary.csv', 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, delimiter=";")
+        for _ in range(num_rows):
+            writer.writerow(get_fake_row())
+
+
+
+if __name__ == '__main__':
+    generate_data()
